69d07db8999e92e8956ff0f5f89dddd61450a17e
KierraWhit <89669379+KierraWhit@users.noreply.github.com>
2024-12-02 17:28:27 -0500

    Update pastry_model.class.php
    added a search method and updated the update pastry method.

--- models/pastry_model.class.php
+++ models/pastry_model.class.php
@@ -39,21 +39,6 @@ class PastryModel {
         return self::$_instance;
     }
 
-    // Method to add a new pastry
-    public function add_pastry($name, $description, $price, $categoryId, $in_menu = 1, $imagePath = null) {
-        // SQL insert statement
-        $sql = "INSERT INTO $this->table (name, description, price, category_id, in_menu, imagePath)
-                VALUES ('$name', '$description', $price, $categoryId, $in_menu, '$imagePath')";
-
-        // Execute the query and return the result
-        $query = $this->dbConnection->query($sql);
-
-        if ($query === true) {
-            return true;
-        }
-        return false;
-    }
-
     // Method to get a pastry by ID
     public function get_pastry_by_id($pastryId) {
         // SQL select statement
@@ -112,13 +97,84 @@ class PastryModel {
         return $pastries;
     }
 
+    //Method to search database for pastries that match words in name. Return an array of pastries if succeed; false otherwise
+    public function search_pastries($terms): bool|array|int{
+        $terms = explode (" ", $terms);
+        //select statement for and search
+        $sql = "SELECT * FROM " . $this->table . " WHERE 1";
+        //Condition statement for each term in the search
+        foreach ($terms as $term){
+            $sql .= "AND name LIKE '%'" . $terms . "'%'";
+        }
+        //execute the query
+        $result = $this->dbConnection->query($sql);
+        //the search failed, return false
+        if (!result){
+            return false;
+        }
+        //the search succeeded, but no pastry was found.
+        if ($result->num_rows ==0)
+            return 0;
+        //the search succeeded, and found at least one pastry
+        //create an array to store all the returned pastries
+        $pastries = array();
+
+        //loop through all rows in returned recordsets
+        while ($row = $result->fetch_object()) {
+                $pastry = new Pastry(
+                    $row->category_id,
+                    $row->name,
+                    $row->description,
+                    $row->image_path,
+                    $row->price,
+                    $row->in_menu
+                );
+            //set the id for the pastry
+                $pastry->setPastryID($row->pastry_id);
+                // add pastry into the array
+                $pastries[] = $pastry;
+            }
+        return $pastries;
+    }
+        
+
     // Method to update a pastry by ID
-    public function update_pastry($pastryId, $name, $description, $price, $categoryId, $in_menu, $imagePath) {
+    public function update_pastry($pastryId): mysqli_result |bool {
+        //if the script did not recieved post data, display an error message and then terminate the script immediately
+        if (!filter_has_var(INPUT_POST, 'name') ||
+            !filter_has_var(INPUT_POST, 'category_id') ||
+            !filter_has_var(INPUT_POST, 'description') ||
+            !filter_has_var(INPUT_POST, 'price') ||
+            !filter_has_var(INPUT_POST, 'image_path') ||
+            !filter_has_var(INPUT_POST, 'in_menu'))
+        {
+            return false;
+        }
+        $name = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'name', FILTER_SANTIZE_STRING)));
+        $categoryId = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'category_id', FILTER_SANTIZE_STRING)));
+        $description = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'description', FILTER_SANTIZE_STRING)));
+        $price = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'price', FILTER_SANTIZE_STRING)));
+        $imagePath = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'image_path', FILTER_SANTIZE_STRING)));
+        $inMenu = $this->dbConnection-> real_escape_string(trim(filter_input(INPUT_POST, 'in_menu', FILTER_SANTIZE_STRING)));
+        
         // SQL update statement
         $sql = "UPDATE $this->table 
-                SET name = '$name', description = '$description', price = $price, category_id = $categoryId,
-                    in_menu = $in_menu, imagePath = '$imagePath'
+                SET name = '$name', category_id = $categoryId, description = '$description', price = $price, 
+                 imagePath = '$imagePath', in_menu = $in_menu
                 WHERE pastry_id = $pastryId";
+
+        // Execute the query and return the result
+        return $this->dbConnection->query($sql);
+
+    }
+
+    //Need to Update these next
+
+    // Method to add a new pastry
+    public function add_pastry($name, $description, $price, $categoryId, $in_menu = 1, $imagePath = null) {
+        // SQL insert statement
+        $sql = "INSERT INTO $this->table (name, description, price, category_id, in_menu, imagePath)
+                VALUES ('$name', '$description', $price, $categoryId, $in_menu, '$imagePath')";
 
         // Execute the query and return the result
         $query = $this->dbConnection->query($sql);
